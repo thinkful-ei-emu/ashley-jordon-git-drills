742f3506a3e446591457b3627ea20bdbf9d7b26c
Jvymvn <Jordon.carter@yahoo.com>
2019-06-03 15:40:41 -0400

    jedi name

---
+++ functionDrills2.js
@@ -0,0 +1,7 @@
+function jediName(firstName, lastName) {
+    const newName1 = firstName.slice(0,2);
+    const newName2 = lastName.slice(0,3);
+    return `${newName2}${newName1}`
+}
+
+console.log(jediName("Ashley", "Jordon"));
