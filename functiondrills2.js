b2c60eb6038ff2ebbad95d226fe32bd88946d5ba
aoelbaum <ashleyoelbaum@gmail.com>
2019-06-03 15:55:51 -0400

    to infinity

--- functiondrills2.js
+++ functiondrills2.js
@@ -1,5 +1,4 @@
 
-
 function jediName(firstName, lastName) 
 { const newName1 = firstName.slice(0,2); 
 const newName2 = lastName.slice(0,3); 
@@ -7,3 +6,21 @@ return `${newName2}${newName1}`
 }
 
 console.log(jediName("Ashley", "Jordon"));
+
+function beyond (num) {
+    if (num === Infinity) {
+       console.log("And beyond");
+    }
+    if (num !== Infinity && num > 0) {
+        console.log("To infinity")
+    }
+    if (num !== Infinity && num < 0) {
+        console.log("To negative infinity")
+    }
+    if (num === 0){
+        console.log("Staying home")
+    }
+    
+};
+
+console.log(beyond(-5));
