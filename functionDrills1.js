23dfb7ba7b9f10fdbb13c38cc011bc5e05bbdb9a
Jvymvn <Jordon.carter@yahoo.com>
2019-06-03 14:43:07 -0400

    #7

--- functionDrills1.js
+++ functionDrills1.js
@@ -1,4 +1,7 @@
 function getYearOfBirth(age){
+    if (age < 0){
+        throw new Error("Age can not be negative");
+    }
     const yearOfBirth = 2019 - age;
     return `I was born ${yearOfBirth}`;
 }
@@ -7,7 +10,14 @@ function createGreeting(name, age) {
     return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
+try {
+    const myAge = getYearOfBirth(-29);
+} catch (Error) {
+    console.error(Error);
+}
+
 const greeting1 = createGreeting();
 console.log(greeting1);
 
-console.log(getYearOfBirth(29));
+const myAge = getYearOfBirth();
+console.log(myAge);
