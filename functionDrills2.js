334f2995eeccea862e977063eda7d1e2298694c6
Jordon A. Carter <jordon.carter@yahoo.com>
2019-06-03 18:46:22 -0400

    Delete functionDrills2.js
    Older file.

--- functionDrills2.js
+++
@@ -1,7 +0,0 @@
-function jediName(firstName, lastName) {
-    const newName1 = firstName.slice(0,2);
-    const newName2 = lastName.slice(0,3);
-    return `${newName2}${newName1}`
-}
-
-console.log(jediName("Ashley", "Jordon"));
