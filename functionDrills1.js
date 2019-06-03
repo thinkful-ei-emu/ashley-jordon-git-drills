ed9d804139c81c517f5bf0ef33c7fc656b1bbb2c
Jvymvn <Jordon.carter@yahoo.com>
2019-06-03 15:10:13 -0400

    #8

--- functionDrills1.js
+++ functionDrills1.js
@@ -10,16 +10,26 @@ function createGreeting(name, age) {
     if (name === undefined && age === undefined){
         throw new Error("Arguments not vailid")
     }
+
+    if (typeof age !== 'number' || typeof name !== 'string') {
+           throw new TypeError("Type not valid");
+        }
     return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
 try {
-    const myAge = getYearOfBirth(-29);
+   getYearOfBirth(-29);
 } catch (Error) {
     console.error(Error);
 }
 
-const greeting1 = createGreeting();
+try {
+    createGreeting("Ashley", "twenty nine");
+} catch (Error) {
+    console.error(Error);
+}
+
+const greeting1 = createGreeting("Jordon", 29);
 console.log(greeting1);
 
 const myAge = getYearOfBirth();
