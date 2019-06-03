c43b2692a2e85717c32a3e941e622b974c621a02
Jvymvn <Jordon.carter@yahoo.com>
2019-06-03 14:29:33 -0400

    #5

--- functionDrills1.js
+++ functionDrills1.js
@@ -1,6 +1,8 @@
 function createGreeting(name, age) {
-    return `Hi, my name is ${name} and I'm ${age} years old`;
+    const yearOfBirth = 2019 - age;
+    return `I was born ${yearOfBirth}`;
 }
 
 const greeting1 = createGreeting();
 console.log(greeting1);
+
